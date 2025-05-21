84fb9dd825b6f280d36aa6c1fc47d857484055b9
adityaRajGit <adityarajcv@outlook.com>
2025-05-22 02:47:32 +0530

    Implement therapist management API with CRUD operations and add therapist routes

---
+++ src/common/helpers/therapist.helper.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+import Therapist from '../models/therapist';
+import BaseHelper from  "./baseHelper.js";
+
+const therapistHelper = new BaseHelper(Therapist);
+
+export default therapistHelper;
