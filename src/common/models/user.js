43658ccce3e72348147f9b2cbbb70d15f545e3fa
AdityaRajTaazaa <aditya.raj@taazaa.com>
2025-06-06 17:27:44 +0530

    Enhance user model and add username generation for new users

--- src/common/models/user.js
+++ src/common/models/user.js
@@ -7,52 +7,64 @@ const userSchema = new Schema({
         required: true,
         trim: true
     },
+    
     email: {
         type: String,
         required: true,
         trim: true
     },
+    
     username:{
         type: String,
-        required: true,
         unique: true,
         trim: true
     },
+    
     phone:{
         type: String,
         minlength: 10,
         maxlength: 10,
     },
+    
     password: {
         type: String,
         required: true
     },
+    
     profile_pic: {
         type: String
     },
+    
     bio: String,
+    
     status : {
         type: String,
         enum: ['active', 'inactive'],
         default: 'active'
     },
+    
     lastLogin : Date,
+    
     isPrivate: {
         type: Boolean,
         default: false
       },
-    blockedUsers: [{
+    
+      blockedUsers: [{
     type: Schema.Types.ObjectId,
     ref: 'User'
     }],
+
     following: [{
         type: Schema.Types.ObjectId,
         ref: 'User'
     }],
+
     followers: [{
         type: Schema.Types.ObjectId,
         ref: 'User'
     }],
+
     is_deleted: {
         type: Boolean,
         default: false 
