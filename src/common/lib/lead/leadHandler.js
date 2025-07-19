060080550a2dc63fc3f42df04bc256d605ada31c
AdityaRajTaazaa <aditya.raj@taazaa.com>
2025-07-19 19:38:38 +0530

    Add integration tests for Lead API and update package dependencies

--- src/common/lib/lead/leadHandler.js
+++ src/common/lib/lead/leadHandler.js
@@ -15,30 +15,36 @@ export async function updateLeadDetailsHandler(input) {
 
 export async function updateLeadStatusHandler(input) {
     try {
-        if (input.status === 'active') {
+        if (input.status === 'client') {
             await leadHelper.deleteObjectById(input.objectId);
             const data = {
-                name: input.updateObject.name,
-                company_mail: input.updateObject.company_mail,
+                name: input.updateObject.company || input.updateObject.name,
+                size: input.updateObject.employees,
                 industry: input.updateObject.industry,
+                company_mail: input.updateObject.email,
+                status: 'in_process',
+                website: input.updateObject.website,
                 address: input.updateObject.address,
-                packageType: input.updateObject.package,
-                size: input.updateObject.size,
-                website: input.updateObject.website
-            }
+                package: input.updateObject.package, // ObjectId
+                webinarsCompleted: 0,
+                webinarsScheduled: 0,
+                visibility: false,
+                is_deleted: false,
+                created_at: new Date(),
+                updated_at: new Date()
+            };
             const addCompany = await addNewCompanyHandler(data);
             return addCompany;
         } else {
             const updateObject = {
-                status: 'inactive'
-            }
+                status: input.status
+            };
             return await leadHelper.directUpdateObject(input.objectId, updateObject);
         }
     } catch (error) {
         console.log(error);
-        throw error
+        throw error;
     }
-
 }
 
 export async function getLeadListHandler(input) {
