faca34281da351f84cb4dbd4d747a7ebb618816f
RishabhRawat2003 <rawatrishabh953@gmail.com>
2025-07-20 12:29:39 +0530

    changes in leads and packages

--- src/server/routes/lead.routes.js
+++ src/server/routes/lead.routes.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import {Router} from 'express';
+import { Router } from 'express';
 
 import {
     addNewLeadHandler,
@@ -9,55 +9,56 @@ import {
     updateLeadDetailsHandler,
     updateLeadStatusHandler
 } from '../../common/lib/lead/leadHandler';
-import { sendContactSupportEmail } from "../../common/util/utilHelper"; 
+import { sendContactSupportEmail } from "../../common/util/utilHelper";
 import responseStatus from "../../common/constants/responseStatus.json";
 import responseData from "../../common/constants/responseData.json";
+import protectRoutes from '../../common/util/protectRoutes';
 
 const router = new Router();
 
-router.route('/list').post(async (req, res) => {
+router.route('/list').post(protectRoutes.verifyAdmin, async (req, res) => {
     try {
-      let filter = {};
-      filter.query = {};
-  
-      const inputData = { ...req.body };
-      if (inputData) {
-        filter.pageNum = inputData.pageNum ? inputData.pageNum : 1;
-        filter.pageSize = inputData.pageSize ? inputData.pageSize : 50;
-  
-        if (inputData.filters) {
-          filter.query = inputData.filters;
+        let filter = {};
+        filter.query = {};
+
+        const inputData = { ...req.body };
+        if (inputData) {
+            filter.pageNum = inputData.pageNum ? inputData.pageNum : 1;
+            filter.pageSize = inputData.pageSize ? inputData.pageSize : 50;
+
+            if (inputData.filters) {
+                filter.query = inputData.filters;
+            }
+        } else {
+            filter.pageNum = 1;
+            filter.pageSize = 50;
         }
-      } else {
-        filter.pageNum = 1;
-        filter.pageSize = 50;
-      }
-  
-      filter.query = { ...filter.query };
-  
-      const outputResult = await getLeadListHandler(filter);
-      res.status(responseStatus.STATUS_SUCCESS_OK);
-      res.send({
-        status: responseData.SUCCESS,
-        data: {
-          leadList: outputResult.list ? outputResult.list : [],
-          leadCount: outputResult.count ? outputResult.count : 0,
-        },
-      });
+
+        filter.query = { ...filter.query };
+
+        const outputResult = await getLeadListHandler(filter);
+        res.status(responseStatus.STATUS_SUCCESS_OK);
+        res.send({
+            status: responseData.SUCCESS,
+            data: {
+                leadList: outputResult.list ? outputResult.list : [],
+                leadCount: outputResult.count ? outputResult.count : 0,
+            },
+        });
     } catch (err) {
-      console.log(err);
-      res.status(responseStatus.INTERNAL_SERVER_ERROR);
-      res.send({
-        status: responseData.ERROR,
-        data: { message: err },
-      });
+        console.log(err);
+        res.status(responseStatus.INTERNAL_SERVER_ERROR);
+        res.send({
+            status: responseData.ERROR,
+            data: { message: err },
+        });
     }
-  });
+});
 
 
 router.route('/new').post(async (req, res) => {
     try {
-       if (!_.isEmpty(req.body)) {
+        if (!_.isEmpty(req.body)) {
             const outputResult = await addNewLeadHandler(req.body.lead);
             res.status(responseStatus.STATUS_SUCCESS_OK);
             res.send({
@@ -103,7 +104,7 @@ router.route('/:id').get(async (req, res) => {
     }
 });
 
-router.route('/:id/update').post( async (req, res) => {
+router.route('/:id/update').post(async (req, res) => {
     try {
         if (!_.isEmpty(req.params.id) && !_.isEmpty(req.body) && !_.isEmpty(req.body.lead)) {
             let input = {
@@ -112,12 +113,12 @@ router.route('/:id/update').post( async (req, res) => {
             }
             const updateObjectResult = await updateLeadDetailsHandler(input);
             res.status(responseStatus.STATUS_SUCCESS_OK);
-                res.send({
-                    status: responseData.SUCCESS,
-                    data: {
-                        lead: updateObjectResult ? updateObjectResult : {}
-                    }
-                });
+            res.send({
+                status: responseData.SUCCESS,
+                data: {
+                    lead: updateObjectResult ? updateObjectResult : {}
+                }
+            });
         } else {
             throw 'no body or id param sent'
         }
@@ -131,7 +132,7 @@ router.route('/:id/update').post( async (req, res) => {
     }
 });
 
-router.route('/:id/update-status').post( async (req, res) => {
+router.route('/:id/update-status').post(protectRoutes.verifyAdmin, async (req, res) => {
     try {
         if (!_.isEmpty(req.params.id) && !_.isEmpty(req.body)) {
             let input = {
@@ -140,12 +141,12 @@ router.route('/:id/update-status').post( async (req, res) => {
             }
             const updateObjectResult = await updateLeadStatusHandler(input);
             res.status(responseStatus.STATUS_SUCCESS_OK);
-                res.send({
-                    status: responseData.SUCCESS,
-                    data: {
-                        lead: updateObjectResult ? updateObjectResult : {}
-                    }
-                });
+            res.send({
+                status: responseData.SUCCESS,
+                data: {
+                    lead: updateObjectResult ? updateObjectResult : {}
+                }
+            });
         } else {
             throw 'no body or id param sent'
         }
@@ -159,7 +160,7 @@ router.route('/:id/update-status').post( async (req, res) => {
     }
 });
 
-router.route('/:id/remove').post(async(req, res) => {
+router.route('/:id/remove').post(async (req, res) => {
     try {
         if (req.params.id) {
             const deletedLead = await deleteLeadHandler(req.params.id);
@@ -184,36 +185,40 @@ router.route('/:id/remove').post(async(req, res) => {
 });
 
 router.post('/add-lead', async (req, res) => {
-  try {
-    const { name, email, phone, company, employees, message } = req.body;
-    if (!name || !email || !phone || !company || !employees) {
-      return res.status(400).json({
-        status: responseData.ERROR,
-        data: { message: 'All fields are required' }
-      });
+    try {
+        const { name, email, phone, company, employees, message, source } = req.body;
+        if (!name || !email || !phone || !company || !employees) {
+            return res.status(400).json({
+                status: responseData.ERROR,
+                data: { message: 'All fields are required' }
+            });
+        }
+        const leadData = {
+            name,
+            email,
+            phone,
+            company,
+            employees,
+            message,
+            source
+        };
+        await addNewLeadHandler(leadData);
+
+        if (source === 'website') {
+            await sendContactSupportEmail({ name, email, phone, company, employees, message });
+        }
+
+        res.status(responseStatus.STATUS_SUCCESS_OK).json({
+            status: responseData.SUCCESS,
+            data: { message: 'Support request sent successfully' }
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(responseStatus.INTERNAL_SERVER_ERROR).json({
+            status: responseData.ERROR,
+            data: { message: err.message || err }
+        });
     }
-    const leadData = {
-      name,
-      email,
-      phone,
-      company,
-      employees,
-      message
-    };
-    await addNewLeadHandler(leadData);
-    
-    await sendContactSupportEmail({ name, email, phone, company, employees, message });
-    res.status(responseStatus.STATUS_SUCCESS_OK).json({
-      status: responseData.SUCCESS,
-      data: { message: 'Support request sent successfully' }
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(responseStatus.INTERNAL_SERVER_ERROR).json({
-      status: responseData.ERROR,
-      data: { message: err.message || err }
-    });
-  }
 });
 
 export default router;
