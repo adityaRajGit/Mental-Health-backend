4ae58edf525f4b77d307f57a3e837c9fd5f49e80
AdityaRajTaazaa <aditya.raj@taazaa.com>
2025-07-28 00:49:25 +0530

    Implement Webinar management functionality with CRUD operations and API routes

---
+++ src/common/helpers/webinar.helper.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+import Webinar from '../models/webinar';
+import BaseHelper from  "./baseHelper.js";
+
+const webinarHelper = new BaseHelper(Webinar);
+
+export default webinarHelper;
